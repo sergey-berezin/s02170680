d8e946eb41cb49e179dcf223a27edb4eba529136
unknown <Nikky>
2020-10-05 11:23:25 +0300

    CancelConsole

--- Parallel/Program.cs
+++ Parallel/Program.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.IO;
-using ModelLib;
 using System.Threading;
+using ModelLib;
 
 namespace Parallel
 {
+    
     class Program
     {
+        static CancellationTokenSource source=new CancellationTokenSource();
+        static void CancelHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            source.Cancel();
+        }
         static void Main(string[] args)
         {
             Model model=new Model();
             CancellationTokenSource source=new CancellationTokenSource();
-            // source.Cancel();
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelHandler);
             model.PredImages(@"..\ModelLib\Samples", Console.OpenStandardOutput(), source.Token); 
             // FileStream fileStream=File.OpenWrite("output.txt");
             // model.PredImages(@"..\ModelLib\Samples", fileStream, source.Token);
